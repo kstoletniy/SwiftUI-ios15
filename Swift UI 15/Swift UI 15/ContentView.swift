1a70cca0672fe71b43e0963bfd537692f079dfd4
Karen <karen.stoletniy@emeritus.org>
2022-06-13 14:16:33 -0300

    Setup project

---
+++ Swift UI 15/Swift UI 15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift UI 15
+//
+//  Created by Karen Stoletniy on 13/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift UI 15/Swift UI 15/Swift_UI_15App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_15App.swift
+//  Swift UI 15
+//
+//  Created by Karen Stoletniy on 13/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
