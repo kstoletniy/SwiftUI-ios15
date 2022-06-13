79df29091f7af8eb9efbcd2c2704cfe096b6413b
Karen <karen.stoletniy@emeritus.org>
2022-06-13 16:07:32 -0300

    Create custom ViewModifires to encapsulate styles

--- Swift UI 15/Swift UI 15/ContentView.swift
+++ Swift UI 15/Swift UI 15/ContentView.swift
@@ -15,9 +15,13 @@ struct ContentView: View {
         .resizable(resizingMode: .stretch)
         .aspectRatio(contentMode: .fit)
         .frame(width: 26.0, height: 26.0)
-        .cornerRadius(20.0)
+        .cornerRadius(10.0)
         .padding(9)
-        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .background(
+          .ultraThinMaterial,
+          in: RoundedRectangle(cornerRadius: 16, style: .continuous)
+        )
+        .strokeStyle(cornerRadius: 16)
       Text("Swift UI")
         .font(.largeTitle)
         .fontWeight(.bold)
@@ -39,13 +43,17 @@ struct ContentView: View {
     .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
     .padding(.vertical, 20.0)
     .frame(height: 350.0)
-    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
-    .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
-    .overlay(
-      RoundedRectangle(cornerRadius: 30, style: .continuous)
-        .stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
-        .blendMode(.overlay)
+    .background(
+      .ultraThinMaterial,
+      in: RoundedRectangle(cornerRadius: 30, style: .continuous)
     )
+    .shadow(
+      color: Color("Shadow").opacity(0.3),
+      radius: 10,
+      x: 0,
+      y: 10
+    )
+    .strokeStyle()
     .padding(.horizontal, 20.0)
     .background(Image("Blob 1").offset(x: 250, y: -100))
     .overlay(
